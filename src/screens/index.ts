d3f8a18a8a922da0d001b640cbc3ac0c68a0e01b
Zhannur <jannuraidynuly@gmail.com>
2022-02-02 18:32:48 +0500

    create screens

---
+++ src/screens/index.ts
@@ -0,0 +1,6 @@
+import SignUp from "./signUp";
+import Login from "./login";
+import Home from "./home";
+import Dashboard from "./dashboard";
+
+export { SignUp, Login, Home, Dashboard }
---
+++ src/screens/signUp.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const SignUp: FC = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Sign Up Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default SignUp;
