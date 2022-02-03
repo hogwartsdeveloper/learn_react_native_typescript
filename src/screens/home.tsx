3808b25c1fc8844fc41297efc0f6bc46f29bfd5c
Zhannur <jannuraidynuly@gmail.com>
2022-02-03 18:53:08 +0500

    creating post

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,18 +1,41 @@
 import firebase from "firebase";
-import { FC } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Button } from "../components";
+import { FC, useState } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import { Button, Input } from "../components";
 
 const Home: FC = () => {
+    const [msg, setMsg] = useState<string | null>(null);
 
     const signOut = () => {
         firebase.auth().signOut();
+    }
+
+    const post = async () => {
+        if(msg) {
+            const data = {
+                msg,
+                timeStamp: Date.now(),
+                approved: false
+            }
+            try {
+                await firebase.firestore().collection('posts').add(data);
+            } catch(e) {
+                console.log(e)
+            }
+            
+        } else {
+            Alert.alert('Missing Fields')
+        }
     }
 
     return (
         <View style={styles.container}>
             <Text>Home Screen</Text>
             <Button title="Sign Out" onPress={signOut} />
+            <View>
+                <Input placeholder="Write Something Here" onChangeText={(text) => setMsg(text)} />
+                <Button title="Post" onPress={post} />
+            </View>
         </View>
     )
 }
